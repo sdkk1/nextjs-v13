a1c7d2c0e6630b6827b8c7d1fa9965653edae0f9
Keita <fast.break.baske03@gmail.com>
2022-12-17 16:11:09 +0900

    remove: pagesディレクトリを削除

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
