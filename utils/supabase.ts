7eb6f2dd7b21a1d92151ba65d29f7080c54f017c
Keita <fast.break.baske03@gmail.com>
2022-12-18 22:21:12 +0900

    add: createBrowserSupabaseClientを作成

---
+++ utils/supabase.ts
@@ -0,0 +1,5 @@
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+import { type Database } from '../database.types'
+
+export default createBrowserSupabaseClient<Database>()
