cc4e1207be6f24cc27c6f115ac59bcc395b0d22f
Keita <fast.break.baske03@gmail.com>
2022-12-18 22:32:44 +0900

    add: TodoDetailPageコンポーネントを作成

---
+++ app/auth/todo-crud/[todoId]/page.tsx
@@ -0,0 +1,44 @@
+import { cookies, headers } from 'next/headers'
+import { notFound } from 'next/navigation'
+
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { format } from 'date-fns'
+
+import type { Database } from '../../../../database.types'
+
+type PageProps = {
+  params: {
+    todoId: string
+  }
+}
+
+const TodoDetailPage = async ({ params }: PageProps) => {
+  const supabase = createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  })
+
+  const { data: todo } = await supabase
+    .from('todos')
+    .select('*')
+    .eq('id', params.todoId)
+    .single()
+
+  if (!todo) {
+    return notFound()
+  }
+
+  return (
+    <div className='mt-16 border-2 p-8'>
+      <p>Task ID: {todo.id}</p>
+      <p>Title: {todo.title}</p>
+      <p>Status: {todo.completed ? 'done' : 'not yet'}</p>
+      <p>
+        Created at:{' '}
+        {todo && format(new Date(todo.created_at), 'yyyy-MM-dd HH:mm:ss')}
+      </p>
+    </div>
+  )
+}
+
+export default TodoDetailPage
