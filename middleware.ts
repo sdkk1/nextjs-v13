963787a2272d2f46c3bbf8e35e861dd6c239bd22
Keita <fast.break.baske03@gmail.com>
2022-12-18 22:34:28 +0900

    add: middlewareを設定

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+const middleware = async (req: NextRequest) => {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareSupabaseClient({ req, res })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session && req.nextUrl.pathname.startsWith('/auth/todo-crud')) {
+    const redirectUrl = req.nextUrl.clone()
+    redirectUrl.pathname = '/auth'
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  return res
+}
+
+export default middleware
