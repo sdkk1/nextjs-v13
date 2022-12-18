6657c52058ac3cff4cf5253a7ddc72cccf436233
Keita <fast.break.baske03@gmail.com>
2022-12-18 22:33:18 +0900

    add: BlogPageコンポーネントを作成

---
+++ app/blogs/page.tsx
@@ -0,0 +1,16 @@
+import RouterBtn from '../components/router-btn'
+
+const BlogPage = () => {
+  return (
+    <div className='m-10 text-center'>
+      <span className='text-lg'>
+        Click a title on the left to view detail 🚀
+      </span>
+      <div className='my-5 flex justify-center'>
+        <RouterBtn />
+      </div>
+    </div>
+  )
+}
+
+export default BlogPage
