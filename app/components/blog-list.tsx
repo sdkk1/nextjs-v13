a1642635d82e23fdf535d749e66b00d0473ac995
Keita <fast.break.baske03@gmail.com>
2022-12-18 22:23:19 +0900

    add: BlogListコンポーネントを作成

---
+++ app/components/blog-list.tsx
@@ -0,0 +1,49 @@
+import type { Database } from '../../database.types'
+
+type Blog = Database['public']['Tables']['blogs']['Row']
+
+const fetchBlogs = async () => {
+  // NOTE: 意図的に遅延させる
+  await new Promise((resolve) => {
+    return setTimeout(resolve, 6000)
+  })
+
+  const res = await fetch(`${process.env.url}/rest/v1/blogs?select=*`, {
+    headers: new Headers({
+      apikey: process.env.apikey as string,
+    }),
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const blogs: Blog[] = await res.json()
+  return blogs
+}
+
+const BlogList = async () => {
+  const blogs = await fetchBlogs()
+
+  return (
+    <div className='p-4'>
+      <p className='mb-4 pb-3 text-xl font-medium underline underline-offset-4'>
+        Blogs
+      </p>
+      <ul className='text-sm'>
+        {blogs?.map((blog) => {
+          return (
+            <li
+              key={blog.id}
+              className='my-1 text-base'
+            >
+              {blog.title}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default BlogList
